5df2854d10a6d8262a3a98f489224b4e8510ac6e
lari <laribelcorderol@gmail.com>
2024-07-31 22:18:17 -0600

    Descripción de los cambios

--- app/Http/Controllers/api/TaskController.php
+++ app/Http/Controllers/api/TaskController.php
@@ -3,26 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
 
-        return Task::select('id', 'name')->get();
+        $tasks = Task::all(['id', 'name']);
+        return response()->json($tasks);
     }
 
-    public function user(){
+    public function user(int $userId){
 
-        
+        if (Auth::user()->id !== $userId) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+
+        $tasks = Task::where('user_id', $userId)->get(['id', 'name']);
+        return response()->json($tasks);
     }
 
 
-    public function update(){
-        
+    public function update(Request $request, Task $task): JsonResponse{
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string'
+            ,
+        ]);
+    
+        $task->update($validatedData);
+    
+        return response()->json($task);
     }
+    
+    public function deleteTask(Request $request, Task $task): JsonResponse
+{
+    
+    $task->delete();
 
+    
+    return response()->json(['message' => 'Task deleted successfully.']);
+} 
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,4 +16,24 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/tasks', [TaskController::class, 'userTasks']);
     Route::patch('/tasks/{task}', [TaskController::class, 'update']);
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+});
+
+
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
 });
